43acc364f408da1b30060df85f54dc3564b8a684
YOUSSEF ADARDOUR <youssefredmi599@gmail.com>
2026-05-25 22:59:09 +0100

    Add:if_statements/exercises/check_even_or_odd.py

---
+++ if_statements/exercises/check_even_or_odd.py
@@ -0,0 +1,12 @@
+# ============================================================
+# Ask the user for a number.
+# Print if the number is even or odd.
+# ============================================================
+
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
