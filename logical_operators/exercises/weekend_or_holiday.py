b1da541cc04a7388bc2669b9846d4212b136fa78
YOUSSEF ADARDOUR <youssefredmi599@gmail.com>
2026-05-31 23:39:18 +0100

    Add : logical_operators/exercises/weekend_or_holiday.py

---
+++ logical_operators/exercises/weekend_or_holiday.py
@@ -0,0 +1,20 @@
+# ============================================================
+# Print True if it is a weekend OR a holiday.
+# ============================================================
+
+is_holiday = True
+is_weekend = True
+
+print("is_holiday:", is_holiday)
+print("is_weekend:", is_weekend)
+
+print("\n--- Logical Operations ---")
+
+print("Weekend AND Holiday:", is_weekend and is_holiday)
+print("NOT Weekend AND NOT Holiday:", not is_weekend and not is_holiday)
+
+print("Weekend OR Holiday:", is_weekend or is_holiday)
+print("NOT Weekend OR NOT Holiday:", not is_weekend or not is_holiday)
+
+print("Weekend AND NOT Holiday:", is_weekend and not is_holiday)
+print("NOT Weekend AND Holiday:", not is_weekend and is_holiday)
